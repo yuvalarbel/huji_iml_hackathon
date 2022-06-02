dd64d4c051dcd64a7f64b8e0b098ffd3df46fc08
Yuval Arbel <yuval@arbels.net>
2022-06-02 15:48:03 +0300

    fixed main.py and created cmnd line args

--- submission/task1/main.py
+++ submission/task1/main.py
@@ -1,22 +1,30 @@
 import argparse
 
-from consts import TRAINING_DATA_PATH, TEST_DATA_PATH
+from consts import TEST_DATA_PATH, TASK_2_TEST_DATES
 from preprocess import preprocess
 from task_1 import run_task_1
 from task_2 import run_task_2
 
 
-def main(training_set, task_1_test_set, task_2_list_of_dates):
+USAGE = "\n%(prog)s\n" \
+        "%(prog)s [-t task_1_test_set] [-d task_2_list_of_dates]\n" \
+        "%(prog)s [--testset task_1_test_set] [--dates task_2_list_of_dates]"
+
+
+def main(task_1_test_set, task_2_list_of_dates):
     """
     Main function
-    :param training_set: relative path to training set
-    :type training_set: str
+
     :param task_1_test_set: relative path to task 1 test set
     :type task_1_test_set: str
     :param task_2_list_of_dates: list of dates to predict for task 2
     :type task_2_list_of_dates: list of pd.date
     """
-    clean_data = preprocess(training_set)
+    print(task_1_test_set)
+    print(task_2_list_of_dates)
+    return
+
+    clean_data = None
 
     try:
         run_task_1(clean_data)
@@ -37,13 +45,14 @@ def parse_args():
     :return: parsed arguments
     :rtype: argparse.Namespace
     """
-    parser = argparse.ArgumentParser(usage="%(prog)s [training_set] [task_1_test_set] [task_2_list_of_dates]")
-    parser.add_argument("training_set", help="relative path to training set", type=str, default=TRAINING_DATA_PATH)
-    parser.add_argument("task_1_test_set", help="relative path to task 1 test set", type=str, default=TEST_DATA_PATH)
-    parser.add_argument("task_2_list_of_dates", help="list of dates to predict for task 2", type=str, nargs="*")
+    parser = argparse.ArgumentParser(usage=USAGE)
+    parser.add_argument('-t', '--testset', dest="task_1_test_set", help="relative path to task 1 test set",
+                        default=TEST_DATA_PATH, type=str, required=False)
+    parser.add_argument('-d', '--dates', dest="task_2_list_of_dates", help="list of dates to predict for task 2",
+                        default=TASK_2_TEST_DATES, nargs="*", type=str, required=False)
     return parser.parse_args()
 
 
 if __name__ == '__main__':
     args = parse_args()
-    main(args.training_set, args.task_1_test_set, args.task_2_list_of_dates)
+    main(args.task_1_test_set, args.task_2_list_of_dates)
