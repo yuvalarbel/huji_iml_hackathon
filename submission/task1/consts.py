59d3352c8c70a631bbdf95ad894593eeefaf6902
Yuval Dycian <yuval.dycian@mail.huji.ac.il>
2022-06-02 23:50:42 +0300

    started working on 2

--- submission/task1/consts.py
+++ submission/task1/consts.py
@@ -1,5 +1,6 @@
 TRAINING_DATA_PATH = 'data/waze_data.csv'
 TEST_DATA_PATH = 'data/waze_take_features.csv'
 TASK_2_TEST_DATES = ['2022/06/05', '2022/06/07', '2022/06/09']
+TASK_2_TRAIN = 'data/task2/waze_data_train.csv'
 
 TEL_AVIV = "תל אביב - יפו"
--- submission/task1/train/task_2_train.py
+++ submission/task1/train/task_2_train.py
@@ -1,12 +1,20 @@
-from consts import TRAINING_DATA_PATH
+from consts import TASK_2_TRAIN
+import pandas as pd
+import numpy as np
+from preprocess_task_2 import preprocess_task_2
 
 
 def train_model(training_data_file_path):
     """
     Train the model
     """
-    pass
+    dt = preprocess_task_2(pd.read_csv(training_data_file_path))
+    dt.update_date_weekday = (dt.update_date_weekday + 2) % 7
+    unique_days = pd.unique(dt.update_date_weekday)
+    
+
 
 
 if __name__ == "__main__":
-    train_model(TRAINING_DATA_PATH)
+    train_model('C:\\Users\\User\\School\\Hackathons\\IML Hackathon '
+                '2022\\huji_iml_hackathon\\submission\\task1\\data\\task2\\waze_data_train.csv')
