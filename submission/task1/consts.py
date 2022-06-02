f0d1b848bbaba4a60d959f9598b7a91e3c2465d7
Yuval Arbel <yuval@arbels.net>
2022-06-02 18:42:26 +0300

    fixed sets and labels

--- submission/task1/consts.py
+++ submission/task1/consts.py
@@ -2,4 +2,9 @@ TRAINING_DATA_PATH = 'data/waze_data.csv'
 TEST_DATA_PATH = 'data/waze_take_features.csv'
 TASK_2_TEST_DATES = ['2022/06/05', '2022/06/07', '2022/06/09']
 
+TASK_1_TRAINING_SET = 'data/task1/train_set.csv'
+
 TEL_AVIV = "תל אביב - יפו"
+
+TASK_1_MODEL_PATH = 'saved_models/task1_model'
+LABEL_COLUMNS = ['linqmap_type', 'linqmap_subtype', 'x', 'y']
