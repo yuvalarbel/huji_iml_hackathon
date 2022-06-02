bc805bb7499891c84514610a3459d8efd2898b91
Yuval Arbel <yuval@arbels.net>
2022-06-02 19:47:59 +0300

    finished baseline predict task1

--- submission/task1/consts.py
+++ submission/task1/consts.py
@@ -3,8 +3,18 @@ TEST_DATA_PATH = 'data/waze_take_features.csv'
 TASK_2_TEST_DATES = ['2022/06/05', '2022/06/07', '2022/06/09']
 
 TASK_1_TRAINING_SET = 'data/task1/train_set.csv'
+TASK_1_TRAINING_LABELS = 'data/task1/train_labels.csv'
 
 TEL_AVIV = "תל אביב - יפו"
 
 TASK_1_MODEL_PATH = 'saved_models/task1_model'
 LABEL_COLUMNS = ['linqmap_type', 'linqmap_subtype', 'x', 'y']
+
+TYPES = ['JAM', 'ROAD_CLOSED', 'WEATHERHAZARD', 'ACCIDENT']
+
+MOST_COMMON_SUBTYPES = {
+    'ACCIDENT': 'ACCIDENT_MINOR',
+    'JAM': 'JAM_STAND_STILL_TRAFFIC',
+    'ROAD_CLOSED': 'ROAD_CLOSED_EVENT',
+    'WEATHERHAZARD': 'HAZARD_ON_ROAD_CAR_STOPPED',
+}
--- submission/task1/train/task_1_train.py
+++ submission/task1/train/task_1_train.py
@@ -1,42 +1,75 @@
+import numpy as np
 import pandas as pd
-# import random forest regression
-from sklearn.ensemble import RandomForestRegressor
+import json
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 import joblib
 
-from consts import TASK_1_TRAINING_SET, TASK_1_MODEL_PATH
+from consts import TASK_1_TRAINING_SET, TASK_1_TRAINING_LABELS, TASK_1_MODEL_PATH, LABEL_COLUMNS, TYPES
 from preprocess_task_1 import preprocess_task_1
 
 
-def train_model(training_data_file_path):
+def train_model(training_data_file_path, training_labels_file_path, save_path, tag=''):
     """
     Train the model
     """
     data = pd.read_csv(training_data_file_path)
-    data = preprocess_task_1(data)
-    model = Task1Model()
-    model.train(data)
+    processed_data = preprocess_task_1(data)
+
+    labels = pd.read_csv(training_labels_file_path)
+
+    model = Task1Model(save_path, tag)
+    model.train(processed_data, labels)
     model.save()
 
 
 class Task1Model(object):
-    """
-    Task 1 Model
-    """
-    def __init__(self, n_estimators=100, max_depth=None):
-        self.model = RandomForestRegressor(n_estimators=n_estimators, max_depth=max_depth)
+    BASE_LABELS = ['linqmap_type', 'x', 'y']
+    TYPE = 'linqmap_type'
+    SUBTYPE = 'linqmap_subtype'
 
-    def train(self, data):
+    def __init__(self, save_path, tag='', n_estimators=100, max_depth=None):
+        self.save_path = save_path
+        self.tag = tag
+        self.models = {
+            'linqmap_type': RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth),
+            'x': RandomForestRegressor(n_estimators=n_estimators, max_depth=max_depth),
+            'y': RandomForestRegressor(n_estimators=n_estimators, max_depth=max_depth),
+
+            'linqmap_subtype_jam': RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth),
+            'linqmap_subtype_road_closed': RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth),
+            'linqmap_subtype_weatherhazard': RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth),
+            'linqmap_subtype_accident': RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth)
+        }
+        self.trained = {label: False for label in self.models}
+
+    def train(self, data, labels):
         """
         Train the model
         """
-        self.model.fit(data)
+        for label in self.BASE_LABELS:
+            self.models[label].fit(data, labels[label])
+            self.trained[label] = True
+
+        for specific_type in TYPES:
+            idxs = np.logical_and(labels[self.TYPE] == specific_type,
+                                  labels[self.SUBTYPE].notna())
+            type_labels = labels[self.SUBTYPE][idxs]
+            if len(type_labels) == 0:
+                continue
+            type_data = data[idxs]
+            model_label = self.SUBTYPE + "_" + specific_type.lower()
+            self.models[model_label].fit(type_data, type_labels)
+            self.trained[model_label] = True
 
     def save(self):
         """
         Save the model
         """
-        joblib.dump(self.model, TASK_1_MODEL_PATH)
+        for name, model in self.models.items():
+            joblib.dump(model, self.save_path + '_' + name + self.tag)
+        with open(self.save_path + '_trained_check' + self.tag, 'w') as f:
+            json.dump(self.trained, f)
 
 
 if __name__ == "__main__":
-    train_model(TASK_1_TRAINING_SET)
+    train_model('../' + TASK_1_TRAINING_SET, '../' + TASK_1_TRAINING_LABELS, '../' + TASK_1_MODEL_PATH, tag='')
